16fb8ed48dfc21bf1c40ad1f169bf08a4ce54110
Mohammed Qedan <qedan@yahoo.com>
2019-04-15 02:07:25 -0700

    added api test starter

--- functions.js
+++ functions.js
@@ -1,27 +1,19 @@
-//api code
-function getAllClients(){
-  /*const myHeaders = new Headers({
-      'Content-Type': 'application/json',
-      'Authorization': '813bf5e36996e6eba47aecfa3433744d'
-  });*/
-  return fetch('http://api.openweathermap.org/data/2.5/weather?zip=', {
-    method: 'GET',
-    mode: 'same-origin',
-    headers: myHeaders,
-  })
-  .then(response => {
-      if (response.status === 200) {
-        return response.json();
-      } else {
-        throw new Error('Something went wrong on api server!');
-      }
-    })
-    .then(response => {
-      console.debug(response);
-    }).catch(error => {
-      console.error(error);
-    });
-    var scriptResponse;
-    return scriptResponse = (response.status)
-  }
-  getAllClients();
+const axios = require('axios');
+
+const functions = {
+  add: (num1, num2) => num1 + num2,
+  isNull: () => null,
+  checkValue: x => x,
+  createUser: () => {
+    const user = { firstName: 'Brad' };
+    user['lastName'] = 'Traversy';
+    return user;
+  },
+  fetchUser: () =>
+    axios
+      .get('https://jsonplaceholder.typicode.com/users/1')
+      .then(res => res.data)
+      .catch(err => 'error')
+};
+
+module.exports = functions;
